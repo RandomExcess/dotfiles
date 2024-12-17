59751f36ee7cc0c357f5059fd51d5f0cbf0fc0ad
Jacob Ågård Bennike <jacobbennike@pm.me>
2024-12-17 11:23:16 +0100

    vterm

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,58 +1,22 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-;;;
 
-;; Identity configuration for GPG, email, etc (uncomment and modify as needed)
+;; Personal Information
 ;; (setq user-full-name "John Doe"
 ;;       user-mail-address "john@doe.com")
 
-;; Search remapping
-(map! :leader
-      (:prefix "s"
-       :desc "Search in buffer" "s" #'+default/search-buffer      ; SPC s s for buffer search
-       :desc "Search in project" "f" #'+default/search-project)    ; SPC s f for project search
-
-      ;; Make [] easier to access
-      (:prefix "c"
-       :desc "Next square bracket" "," (cmd! (insert "[]") (backward-char))
-       :desc "Next curly brace" "." (cmd! (insert "{}") (backward-char)))
-
-      ;; Make commonly used symbols more accessible
-      (:prefix "i"
-       :desc "Insert pipe" "i" (cmd! (insert "|"))
-       :desc "Insert backslash" "b" (cmd! (insert "\\")))
-
-      ;; Easier access to common commands that might use special characters
-      (:prefix "t"
-       :desc "Toggle comment" "c" #'comment-line
-       :desc "Toggle fold" "f" #'evil-toggle-fold))
-
-;; Make æ, ø, and å useful in normal mode
-(map! :n "æ" #'evil-first-non-blank
-      :n "ø" #'evil-end-of-line
-      :n "å" #'evil-backward-word-begin)
-
-;; Save on ESC (exit from insert mode)
-(add-hook 'evil-insert-state-exit-hook
-          (lambda ()
-            (call-interactively #'save-buffer)))
-
-;; maximed for macos
+;; Basic UI Configuration
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'macchiato)
+(load-theme 'catppuccin t)
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/org/")
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; Font Configuration
 (setq doom-font (font-spec :family "GohuFont 14 Nerd Font Mono" :size 14)
       doom-variable-pitch-font (font-spec :family "GohuFont 14 Nerd Font Mono" :size 15))
 
-;; Theme Configuration
-(setq doom-theme 'catppuccin)
-(setq catppuccin-flavor 'macchiato)
-(load-theme 'catppuccin t)
-
-;; Basic UI Configuration
-(setq display-line-numbers-type 'relative)
-(setq org-directory "~/org/")
-
-;; Clipboard Configuration
+;; System Configuration
 (setq select-enable-clipboard t
       x-select-enable-clipboard-manager t)
 
@@ -60,7 +24,7 @@
 (setq evil-vsplit-window-right t
       evil-split-window-below t)
 
-;; Auto-save and backup settings
+;; Auto-save and Backup Configuration
 (setq auto-save-default t
       make-backup-files t
       backup-by-copying t
@@ -78,7 +42,7 @@
   (setq treemacs-width 35
         treemacs-position 'left))
 
-;; Company (Completion) Configuration
+;; Completion (Company) Configuration
 (after! company
   (setq company-idle-delay 0.1
         company-minimum-prefix-length 1)
@@ -91,43 +55,74 @@
         lsp-completion-provider :capf
         lsp-completion-show-detail t
         lsp-completion-show-kind t)
-
   (setq lsp-ui-doc-enable t
         lsp-ui-doc-position 'at-point
         lsp-ui-sideline-enable t
         lsp-ui-sideline-show-diagnostics t
         lsp-ui-sideline-show-hover t))
 
-;; Go Mode Configuration
+;; Programming Language Configurations
+;; Go
 (after! go-mode
-  ;; Format and Import Handling
   (add-hook! 'before-save-hook #'lsp-format-buffer t t)
   (add-hook! 'before-save-hook #'lsp-organize-imports t t)
   (add-hook! go-mode-hook
     (add-hook 'before-save-hook #'gofmt-before-save nil t))
-
-  ;; Go Tools Configuration
   (setq gofmt-command "goimports"
-        go-packages-function 'go-packages-go-list)
-
-  ;; Testing Configuration
-  (setq go-test-args "-v -race"
+        go-packages-function 'go-packages-go-list
+        go-test-args "-v -race"
         gotest-verbose t))
 
-;; Key Bindings (add your custom keybindings here)
-(map! :leader
-      (:prefix "b"
-       :desc "Kill buffer" "d" #'kill-current-buffer)
-      (:prefix "w"
-       :desc "Split window right" "v" #'evil-window-vsplit
-       :desc "Split window below" "s" #'evil-window-split))
-
-;; Additional Language-Specific Configurations
+;; Python
 (after! python-mode
   (setq python-indent-offset 4))
 
+;; JavaScript
 (after! js2-mode
   (setq js2-basic-offset 2
         js-indent-level 2))
 
-;; End of config.el
+;; Key Bindings Configuration
+;; Save on ESC
+(add-hook 'evil-insert-state-exit-hook
+          (lambda ()
+            (call-interactively #'save-buffer)))
+
+;; Leader Key Bindings
+(map! :leader
+      ;; Search bindings
+      (:prefix "s"
+       :desc "Search in buffer" "s" #'+default/search-buffer
+       :desc "Search in project" "f" #'+default/search-project)
+      
+      ;; Code bindings
+      (:prefix "c"
+       :desc "Next square bracket" "," (cmd! (insert "[]") (backward-char))
+       :desc "Next curly brace" "." (cmd! (insert "{}") (backward-char)))
+      
+      ;; Insert bindings
+      (:prefix "i"
+       :desc "Insert pipe" "i" (cmd! (insert "|"))
+       :desc "Insert backslash" "b" (cmd! (insert "\\")))
+      
+      ;; Toggle bindings
+      (:prefix "t"
+       :desc "Toggle comment" "c" #'comment-line
+       :desc "Toggle fold" "f" #'evil-toggle-fold)
+      
+      ;; Buffer bindings
+      (:prefix "b"
+       :desc "Kill buffer" "d" #'kill-current-buffer)
+      
+      ;; Window bindings
+      (:prefix "w"
+       :desc "Split window right" "v" #'evil-window-vsplit
+       :desc "Split window below" "s" #'evil-window-split)
+      
+      ;; Terminal bindings
+      :desc "Open vterm" "v" #'vterm)
+
+;; Special character bindings
+(map! :n "æ" #'evil-first-non-blank
+      :n "ø" #'evil-end-of-line
+      :n "å" #'evil-backward-word-begin)
